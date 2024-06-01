aae967b4de949391ea8b9a2afe68a5409462491c
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-06-01 23:33:16 +0900

    chore: inital project

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+console.log('about')
+</script>
+
+<template>
+  <h1>About</h1>
+</template>
