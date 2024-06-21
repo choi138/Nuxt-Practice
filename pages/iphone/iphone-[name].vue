06ad652a0c22329a3f719ed3e0cb69481ec744e1
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-06-21 19:53:02 +0900

    feat(pages): add dynamic routes

---
+++ pages/iphone/iphone-[name].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+const name = computed(() => route.params.name.replaceAll('-', ' '))
+</script>
+
+<template>
+  <div>
+    I iphone {{ name }}
+  </div>
+</template>
